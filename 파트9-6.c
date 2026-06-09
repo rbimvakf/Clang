327d860b614dc3d3951be107a79252586aac86be
김민관 <rbimvakf@gmail.com>
2026-06-09 11:59:02 +0900

    Update 파트9-6.c

--- 파트9-6.c
+++ 파트9-6.c
@@ -5,7 +5,7 @@ int main()
 	char* cp = "C is a computer programming language.";
 	printf("%s\n", cp);
 	printf("%c\n", *(cp+7));
-	printf("%c\n", cp[10]);
+	printf("%c", cp[10]);
 	/*
 	* C is a computer programming language.
 	* c
