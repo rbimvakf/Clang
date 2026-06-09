79543af47e57bbc79ccc2d76b06eec4fbb03d671
김민관 <rbimvakf@gmail.com>
2026-06-09 18:56:13 +0900

    Update 파트 8-2.c

--- 파트 8-2.c
+++ 파트 8-2.c
@@ -5,6 +5,8 @@ int main() {
 	float b[4] = { 0.5, 1.5, 2.5, 3.5 };
 	char c[20] = "C programming";
 	double d[4] = { 1.25, 2.25, 3.25, 4.25 };
+	
 	printf("%d, %d, %d, %d", (int)sizeof(a), (int)sizeof(b), (int)sizeof(c), (int)sizeof(d)); // 12, 16, 20, 32
+	
 	return 0;
 }
