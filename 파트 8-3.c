4d507f5b739621c2eeb32d7b503e8016e05809a2
김민관 <rbimvakf@gmail.com>
2026-06-09 18:57:34 +0900

    Update 파트 8-3.c

--- 파트 8-3.c
+++ 파트 8-3.c
@@ -2,7 +2,6 @@
 
 int main() {
 	float x[4] = { 1.5, 2.5, 3.5, 4.5 };
-	
 	printf("%f\n", x[0]);
 	printf("%f\n", x[2]);
 	printf("%f\n", x[0] + x[2]);
@@ -13,6 +12,5 @@ int main() {
 	* 5.000000
 	* 5.250000
 	*/
-	
 	return 0;
 }
