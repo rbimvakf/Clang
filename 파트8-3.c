d7383d6cfbb85605fa6f1a38650266f2b05550f1
김민관 <rbimvakf@gmail.com>
2026-06-09 12:34:30 +0900

    Create 파트8-3.c

---
+++ 파트8-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	float x[4] = { 1.5, 2.5, 3.5, 4.5 };
+	printf("%f\n", x[0]);
+	printf("%f\n", x[2]);
+	printf("%f\n", x[0] + x[2]);
+	printf("%f\n", x[0] * x[2]);
+	/*
+	* 1.500000
+	* 3.500000
+	* 5.000000
+	* 5.250000
+	*/
+	return 0;
+}
