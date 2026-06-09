6e518ce080ed9afc0e34117eae2e17835cacbd9b
김민관 <rbimvakf@gmail.com>
2026-06-09 16:19:04 +0900

    Update 기말평가 4번.c

--- 기말평가 4번.c
+++ 기말평가 4번.c
@@ -5,7 +5,7 @@ int main() {
 	printf("소문자로 입력 : ");
 	scanf_s("%s", str, (int)sizeof(str));
 	for (int j = 0; j < strlen(str); j++)
-		str[j] = str[j] - 32; //str[j] -= 32;
+		str[j] = str[j] - 32; // str[j] -= 32;
 	printf("%s\n", str);
 	return 0;
 }
