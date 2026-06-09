60b78f053aadcaa76d145e3c4c00d17bedfc85b5
김민관 <rbimvakf@gmail.com>
2026-06-09 11:39:56 +0900

    Create 파트9-4.c

---
+++ 파트9-4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+	int* p;
+	int n[] = { 100,200,300,400,500 };
+	p = &n[0];
+	printf("%d, %d\n", n[0], *p);
+	printf("%d, %d\n", n[2], *(p+2));
+	/*
+	* 100, 100
+	* 300, 300
+	*/
+	return 0;
+}
