4d97c7e3ed70189e3b1c15f94f0b8bad3882be3e
김민관 <rbimvakf@gmail.com>
2026-06-09 12:29:05 +0900

    Create 파트8-2.c

---
+++ 파트8-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int a[3] = { 1, 3, 5 };
+	float b[4] = { 0.5, 1.5, 2.5, 3.5 };
+	char c[20] = "C programming";
+	double d[4] = { 1.25, 2.25, 3.25, 4.25 };
+	printf("%d, %d, %d, %d", (int)sizeof(a), (int)sizeof(b), (int)sizeof(c), (int)sizeof(d)); // 12, 16, 20, 32
+	return 0;
+}
