5148d9a72731e5cb8af6fa52aaef011503edf069
김민관 <rbimvakf@gmail.com>
2026-06-09 15:51:25 +0900

    Create 기말평가 8번.c

---
+++ 기말평가 8번.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+func(int a[6], int b[2][3]){
+	for (int i = 0; i < 6; i++) 
+		printf("%d ", a[i]);
+	printf("\n");
+
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 3; j++)
+			printf("%d ", b[i][j]);
+}
+
+int main() {
+	int arr1[6] = { 10, 20, 30, 40, 50, 60 };
+	int arr2[2][3] = { {100, 200, 300}, {400, 500, 600} };
+	func(arr1, arr2);
+}
