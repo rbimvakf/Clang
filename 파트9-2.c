e64d340c3ac8de33425f51d71c46ab7c5fd72f1a
김민관 <rbimvakf@gmail.com>
2026-06-09 11:57:03 +0900

    Create 파트9-2.c

---
+++ 파트9-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+	int n = 11;
+	double f = 3.14;
+	int* pn = &n;
+	double* pf = &f;
+	printf("%d, %f\n", n, f);
+	printf("%d, %f\n", *pn, *pf);
+	printf("%f\n", ((float)n+f));
+	/*
+	* 11, 3.140000
+	* 11, 3.140000
+	* 14.140000
+	*/
+	return 0;
+}
